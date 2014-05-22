0a75ba9e60f0f388ab2a120ed1cda25f55cc5887
rakomwolvesbane <robbiemoore4698@gmail.com>
2014-05-22 13:33:48 -0400

    Start Menu
    The basic beginnings of the GUI. Very basic. Almost non-existent.

---
+++ Settlers of Catan/src/GameController.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+public class GameController {
+	private static JFrame frame;
+	private static StartMenu start_menu;
+	//private MainGameGUI game_gui;
+	private static int num_players;
+	private static int victory_points;
+	
+	public static void main(String[] args) {
+		/*String message = "This game is still in progress.\nDon't worry though, we have highly trained specialists hard at work";
+		JOptionPane.showMessageDialog(null, message);*/
+		createStartMenu();
+	}
+
+	HumanPlayer[] players;
+
+	private static void createStartMenu(){
+		frame = new JFrame("Settlers of Catan");
+	    frame.setLocation(300,150);
+	    frame.setSize(600,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		start_menu = new StartMenu();
+		frame.getContentPane().add(start_menu);
+		frame.setVisible(true);
+	}
+	public static void beginGame(int x){
+		start_menu.setVisible(false);
+		num_players = x;
+		if(num_players==3)
+			victory_points=10;
+		else
+			victory_points=12;
+		frame.getContentPane().removeAll();
+	}
+	
+	private static void gameplay() {
+
+	}
+
+	private static void endGame() {
+
+	}
+}
