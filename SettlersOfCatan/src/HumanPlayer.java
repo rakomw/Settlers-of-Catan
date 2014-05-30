358d5667166061ec331a9ef300fc2bbe0b80a02f
rakomwolvesbane <robbiemoore4698@gmail.com>
2014-05-29 23:06:47 -0400

    Why am I trying to code at night without caffeine
    One last fix before I just go to sleep because I keep making small
    errors and not noticing them

--- SettlersOfCatan/src/HumanPlayer.java
+++ SettlersOfCatan/src/HumanPlayer.java
@@ -4,89 +4,89 @@ import java.util.ArrayList;
 import java.awt.Color;
 
 public class HumanPlayer {
-	public HumanPlayer(Color in_color) {
-		color = in_color;
-	}
+   public HumanPlayer(Color in_color) {
+      color = in_color;
+   }
 
-	public void takeTurn() {
-		resourceProduction();
-		//tradeAndBuild();
-	}
+   public void takeTurn() {
+      resourceProduction();
+   	//tradeAndBuild();
+   }
 
-	public final Color color;
-	private ArrayList<TownNode> towns = new ArrayList<TownNode>();
-	private ArrayList<RoadNode> roads = new ArrayList<RoadNode>();
-	private ArrayList<Integer> hand = new ArrayList<Integer>();
+   public final Color color;
+   private ArrayList<TownNode> towns = new ArrayList<TownNode>();
+   private ArrayList<RoadNode> roads = new ArrayList<RoadNode>();
+   private ArrayList<Integer> hand = new ArrayList<Integer>();
    private ArrayList<Card> development = new ArrayList<Card>();
    private int knights;
-	private boolean longest_road;
-	private boolean largest_army;
+   private boolean longest_road;
+   private boolean largest_army;
 
-	private void resourceProduction() {
-		int roll = (int)(Math.random() * 6) + (int)(Math.random() * 6) + 2;
-		for (TownNode town : towns) {
-			for (Tile tile : town.getAdjacentTiles()) {
-				if (tile.roll == roll && !tile.has_robber) {
-				  if (town.getBuildLevel() == 2) {
-				    hand.add(tile.resource);
-				  }
-					hand.add(tile.resource);
-				}
-			}
-		}
-	}
-	public ArrayList<Integer> get_ports() {
-		ArrayList<Integer> ports = new ArrayList<Integer>();
+   private void resourceProduction() {
+      int roll = (int)(Math.random() * 6) + (int)(Math.random() * 6) + 2;
+      for (TownNode town : towns) {
+         for (Tile tile : town.getAdjacentTiles()) {
+            if (tile.roll == roll && !tile.has_robber) {
+               if (town.getBuildLevel() == 2) {
+                  hand.add(tile.resource);
+               }
+               hand.add(tile.resource);
+            }
+         }
+      }
+   }
+   public ArrayList<Integer> get_ports() {
+      ArrayList<Integer> ports = new ArrayList<Integer>();
       for(TownNode t: towns){
-			if (t.get_trader()) {
-				ports.add(t.get_trade_stats());
-			}
-		}
+         if (t.get_trader()) {
+            ports.add(t.get_trade_stats());
+         }
+      }
       return ports;
-	}
+   }
 
-	public int get_roads() {
-	  return roads.size();
-	}
-	public int get_knights() {
-		return knights;
-	}
-	public int get_points() {
-		int points = 0;
-		for (int i=0;i<towns.size();i++) {
-			points += towns.get(i).getBuildLevel();	
-		}
-		if (longest_road == true) {
-			points++;
-			points++;
-		}
-		if (largest_army == true) {
-			points++;
-			points++;
-		}
-		return points;
-	}
+   public int get_roads() {
+      return roads.size();
+   }
+   public int get_knights() {
+      return knights;
+   }
+   public int get_points() {
+      int points = 0;
+      for (int i=0;i<towns.size();i++) {
+         points += towns.get(i).getBuildLevel();	
+      }
+      if (longest_road == true) {
+         points++;
+         points++;
+      }
+      if (largest_army == true) {
+         points++;
+         points++;
+      }
+      return points;
+   }
 	//private void tradeAndBuild() { //use this to call menus to get inputs for seperate helper functions?
      //call menus as players want and get function calls for below
 	//}
-	private void build_road(RoadNode rode) {
-	    try {
-	    rode.buildUp(this);
-		 }
-		 catch(Exception e) {
-
-		 }
-	}
-	private void build_town(TownNode toun) {
-	    try { 
-		 toun.buildUp(this);
-		 }
-		 catch(Exception e) {
-
-		 }
-	}
+   private void build_road(RoadNode rode) {
+      try {
+         rode.buildUp(this);
+      }
+      catch(Exception e) {
+      
+      }
+   }
+   private void build_town(TownNode toun) {
+      try { 
+         toun.buildUp(this);
+      }
+      catch(Exception e) {
+      
+      }
+   }
 	
-	public void trade(int[] to_remove,int[] addition){
+   public boolean trade(int[] to_remove,int[] addition){
       if(hasResources(to_remove)){
          for(int i:to_remove)
             hand.remove(Integer.valueOf(i));
@@ -94,8 +94,12 @@ public class HumanPlayer {
             from interpreting it as an index*/
          for(int i:addition)
             hand.add(i);
+            
+         return true;
       }
-	}
+      else 
+         return false;
+   }
    
    private boolean hasResources(int[] removing){
       int[] count_taking = {0,0,0,0,0};
@@ -111,9 +115,9 @@ public class HumanPlayer {
             
       return true;
    }
-	public void addDevelopmentCard(Card dev) {
-	  development.add(dev);
-	}
+   public void addDevelopmentCard(Card dev) {
+      development.add(dev);
+   }
    
 	/*private void build_ship() {
 	
