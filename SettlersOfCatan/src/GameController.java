fcc786e2fa8cb592299e651a4476156848308df9
rakomwolvesbane <robbiemoore4698@gmail.com>
2014-06-09 11:15:02 -0400

    Minor controller change
    end of game is now less error-prone, frame is made in main so that
    repeat runs don’t cause issues

--- SettlersOfCatan/src/GameController.java
+++ SettlersOfCatan/src/GameController.java
@@ -11,14 +11,14 @@ public class GameController {
    private static JMenuBar menu_bar;
 	
 	public static void main(String[] args) {
+      frame = new JFrame("Settlers of Catan");
+	   frame.setLocation(150,0);
+	   frame.setSize(1000,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		createStartMenu();
 	}
 
 	private static void createStartMenu(){
-		frame = new JFrame("Settlers of Catan");
-	   frame.setLocation(150,0);
-	   frame.setSize(1000,600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		start_menu = new StartMenu();
 		frame.getContentPane().add(start_menu);
 		frame.setVisible(true);
@@ -29,13 +29,14 @@ public class GameController {
       frame.getContentPane().removeAll();
       menu_bar = new GameMenuBar(p);
       frame.setJMenuBar(menu_bar); 
-      game_gui = new GameGUI();
-      frame.getContentPane().add(game_gui);
+      //game_gui = new GameGUI();
+      //frame.getContentPane().add(game_gui);
    }
    
    public static void endGame(HumanPlayer p){
-      game_gui.setVisible(false);
+      //game_gui.setVisible(false);
       frame.getContentPane().removeAll();
+      frame.setJMenuBar(null);
       
       JPanel end = new JPanel();
       end.setLayout(new FlowLayout());
@@ -62,6 +63,8 @@ public class GameController {
    
    private static class playAgainListener implements ActionListener{
       public void actionPerformed(ActionEvent e){
+         frame.setVisible(false);
+         frame.getContentPane().removeAll();
          createStartMenu();
       }
   }
